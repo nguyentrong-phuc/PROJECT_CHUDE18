186fb96ed47516b0597641a55886032ff7296b3b
nguyentrong-phuc <112928938+nguyentrong-phuc@users.noreply.github.com>
2022-12-27 23:33:12 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-import streamlit as st
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import os
@@ -16,5 +15,41 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import load_img, img_to_array
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-st.title('Auto_Translator App')
-IPtype = st.radio("Input Type",('Microphone', 'Text', 'Audio File'),horizontal=True)
+train_data=ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+training=train_data.flow_from_directory('/content/drive/MyDrive/AI/BTVN/Food/Train',target_size=(150,150), batch_size=32, class_mode='categorical')
+validation=train_data.flow_from_directory('/content/drive/MyDrive/AI/BTVN/Food/Validation',target_size=(150,150), batch_size=32, class_mode='categorical')
+# Tạo ra mạng CNN để train mô hình
+model=Sequential()
+model.add(Conv2D(32,(3,3), activation='relu', kernel_initializer='he_uniform', padding='same',input_shape=(150,150,3))) 
+model.add(Conv2D(32,(3,3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+model.add(MaxPooling2D(2,2))
+model.add(Conv2D(64,(3,3), activation='relu', kernel_initializer='he_uniform', padding='same')) 
+model.add(Conv2D(64,(3,3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+model.add(MaxPooling2D(2,2))
+model.add(Conv2D(128,(3,3), activation='relu', kernel_initializer='he_uniform', padding='same')) 
+model.add(Conv2D(128,(3,3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+model.add(MaxPooling2D(2,2))
+# Duỗi thẳng dữ liệu 
+model.add(Flatten())
+model.add(Dense(128,activation='relu',kernel_initializer='he_uniform'))
+model.add(Dropout(0.2))
+model.add(Dense(10,activation='softmax'))
+model.summary()
+# Biên dịch 
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+history=model.fit(training,epochs=40,batch_size=128,verbose=1, validation_data=validation)
+# Lưu 
+model.save("Food_VN.h5")
+model_1=load_model('Food_VN.h5')
+# Test
+test="path input image"
+img=load_img(test,target_size=(150,150))
+plt.imshow(img)
+img=img_to_array(img)
+img=img.astype('float32')
+img=img/255
+img=np.expand_dims(img,axis=0)
+result=(model_1.predict(img).argmax())
+class_name=['BANH CUON','BANH MI','BANH PIA','BANH TET','BANH XEO','BUN DAU MAM TOM','CA KHO TO','COM TAM','NEM','PHO']
+print(class_name[result])
+plt.show()
